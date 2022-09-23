fe188558f9452bb7253f4172269b31ee60750f88
Marc LANTOL <marc.lantol@gmail.com>
2022-09-23 15:32:44 +0200

    Create auth middleware and refacto app routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,30 +2,35 @@ const express = require("express");
 const fs = require("fs");
 const path = require("path");
 const cors = require("cors");
-const router = require("./router");
+const cookieParser = require("cookie-parser");
+const auth = require("./middlewares/auth");
+require("dotenv");
 
 const app = express();
 
-// use some application-level middlewares
+app.use(cookieParser());
+
 app.use(
   cors({
     origin: process.env.FRONTEND_URL ?? "http://localhost:3000",
+    credentials: true,
     optionsSuccessStatus: 200,
   })
 );
 
 app.use(express.json());
-
-// Serve the public folder for public resources
 app.use(express.static(path.join(__dirname, "../public")));
-
-// Serve REACT APP
 app.use(express.static(path.join(__dirname, "..", "..", "frontend", "dist")));
 
-// API routes
+const router = express.Router();
+const userRouter = require("./routes/userRouter");
+const authRouter = require("./routes/authRouter");
+
+router.use("/user", auth, userRouter);
+router.use("/auth", authRouter);
+
 app.use(router);
 
-// Redirect all requests to the REACT app
 const reactIndexFile = path.join(
   __dirname,
   "..",
@@ -41,5 +46,4 @@ if (fs.existsSync(reactIndexFile)) {
   });
 }
 
-// ready to export
 module.exports = app;
