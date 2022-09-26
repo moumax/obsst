8083b84c9f2d9c784436511a62a8e31454ee3e3f
Marc LANTOL <marc.lantol@gmail.com>
2022-09-26 15:05:56 +0200

    Create login page with credentials inputs

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
-  return <div>Login</div>;
+  // eslint-disable-next-line no-unused-vars
+  const [mail, setMail] = useState("");
+  // eslint-disable-next-line no-unused-vars
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+  return (
+    <div>
+      <input
+        type="text"
+        id="user"
+        name="user"
+        placeholder="Votre email"
+        onChange={(e) => setMail(e.target.value)}
+      />
+      <input
+        type="password"
+        id="pass"
+        name="pass"
+        placeholder="Votre mot de passe"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button type="submit">Se connecter</button>
+      <button type="button" onClick={() => navigate("/")}>
+        Retour à la page d'accueil
+      </button>
+    </div>
+  );
 }
