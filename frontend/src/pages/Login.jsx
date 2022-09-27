1a6a82ff7dbd3fd444c061ecc3921b0aec03f3c7
Marc LANTOL <marc.lantol@gmail.com>
2022-09-27 13:02:42 +0200

    Create disconnect button and clear the local storage to delete the token Login is working 100%

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,32 +1,78 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import userAPI from "../services/userAPI";
+import CurrentUserContext from "../contexts/userContext";
 
 export default function Login() {
-  // eslint-disable-next-line no-unused-vars
   const [mail, setMail] = useState("");
-  // eslint-disable-next-line no-unused-vars
   const [password, setPassword] = useState("");
+  const { setUser } = useContext(CurrentUserContext);
   const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (mail && password) {
+      userAPI
+        .post("/api/auth/login", { mail, password })
+        .then((res) => {
+          toast.success("Vous êtes connecté !");
+          setUser(res.data);
+          localStorage.setItem("user", JSON.stringify(res.data));
+          navigate("/");
+        })
+        .catch(() =>
+          toast.warning("Votre email ou votre mot de passe est faux")
+        );
+    }
+  };
+
+  const handleDisconnect = () => {
+    userAPI
+      .get("api/auth/logout")
+      .then(() => {
+        localStorage.clear();
+        setUser(undefined);
+        navigate("/");
+        toast.warning("Vous êtes déconnecté !");
+      })
+      .catch((err) => {
+        console.error("La déconnexion ne fonctionne pas");
+        console.error(err.message);
+      });
+  };
+
   return (
     <div>
-      <input
-        type="text"
-        id="user"
-        name="user"
-        placeholder="Votre email"
-        onChange={(e) => setMail(e.target.value)}
-      />
-      <input
-        type="password"
-        id="pass"
-        name="pass"
-        placeholder="Votre mot de passe"
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button type="submit">Se connecter</button>
-      <button type="button" onClick={() => navigate("/")}>
-        Retour à la page d'accueil
-      </button>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          id="user"
+          name="user"
+          placeholder="Votre email"
+          onChange={(e) => setMail(e.target.value)}
+        />
+        <input
+          type="password"
+          id="pass"
+          name="pass"
+          placeholder="Votre mot de passe"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Se connecter</button>
+        <button type="button" onClick={() => navigate("/")}>
+          Retour à la page d'accueil
+        </button>
+        <button
+          type="button"
+          onClick={() => {
+            handleDisconnect();
+          }}
+        >
+          Déconnection
+        </button>
+      </form>
     </div>
   );
 }
