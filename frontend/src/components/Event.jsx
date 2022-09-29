67ec1ded5d22ef8943e4b8af965a731182e39111
Marc LANTOL <marc.lantol@gmail.com>
2022-09-29 15:18:28 +0200

    Delete button is working & conditionnaly displayed

--- frontend/src/components/Event.jsx
+++ frontend/src/components/Event.jsx
@@ -1,15 +1,37 @@
-import { useNavigate } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
+import { useEffect, useState, useContext } from "react";
+import { toast } from "react-toastify";
 import userAPI from "../services/userAPI";
+import CurrentUserContext from "../contexts/userContext";
 
 function Event() {
   const navigate = useNavigate();
   const [event, setEvent] = useState([]);
+  const { user } = useContext(CurrentUserContext);
+  const location = useLocation();
 
   const getEvent = () => {
     userAPI.get("/api/events").then((response) => {
       setEvent(response.data[0]);
     });
+  };
+
+  const deleteEvent = (id) => {
+    if (user) {
+      userAPI
+        .delete(`/api/events/${id}`)
+        .then(() => {
+          setEvent(
+            event.filter((data) => {
+              return data.id !== id;
+            })
+          );
+          toast.success("Evènement supprimé avec succes");
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    } else toast.warning("Vous n'êtes pas connecté !");
   };
 
   useEffect(() => {
@@ -28,6 +50,11 @@ function Event() {
               <p className="event-description">{data.description}</p>
               <p className="event-site">{data.site}</p>
             </li>
+            {user && location.pathname === "/administration" && (
+              <button type="submit" onClick={() => deleteEvent(data.id)}>
+                Supprimer l'event {data.title}
+              </button>
+            )}
           </ul>
         ))}
       </div>
