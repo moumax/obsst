b5cd0e2326c9bd133e7dca638cdf7dacb2149967
Marc LANTOL <marc.lantol@gmail.com>
2022-10-08 07:40:45 +0200

    Try to make the seach button active

--- frontend/src/components/AllCameras.jsx
+++ frontend/src/components/AllCameras.jsx
@@ -1,15 +1,12 @@
-/* eslint-disable no-unused-vars */
 import { useContext, useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
-import Select from "react-select";
 import CurrentUserContext from "../contexts/userContext";
 import userAPI from "../services/userAPI";
 
 export default function DisplayCameras() {
   const [cameras, setCameras] = useState([]);
-  const [searchTerm, setSearchTerm] = useState("");
-  const [display, setDisplay] = useState(false);
+  const [value, setValue] = useState("");
   const { user } = useContext(CurrentUserContext);
   const navigate = useNavigate();
 
@@ -19,6 +16,7 @@ export default function DisplayCameras() {
     });
   };
 
+  // eslint-disable-next-line no-unused-vars
   const deleteCamera = (id) => {
     if (user) {
       userAPI
@@ -41,171 +39,54 @@ export default function DisplayCameras() {
     getCameras();
   }, []);
 
-  const searcher = (e) => {
-    const { value } = e.target;
-    setSearchTerm(value);
-    if (value === "") setDisplay(false);
+  const onChange = (event) => {
+    setValue(event.target.value);
   };
 
-  const handleDisplay = (e) => {
-    setDisplay(true);
+  const onSearch = (searchTerm) => {
+    setValue(searchTerm);
+    // console.log(value);
+  };
+
+  const write = () => {
+    return value;
   };
 
   return (
     <div>
-      <button type="submit" onClick={() => navigate("create-camera")}>
-        Créer une nouvelle camera
-      </button>
-      <input type="text" placeholder="Rechercher" onChange={searcher} />
+      <div>
+        <button type="submit" onClick={() => navigate("create-camera")}>
+          Créer une nouvelle camera
+        </button>
+        <input
+          type="text"
+          value={value}
+          placeholder="Rechercher"
+          onChange={onChange}
+        />
+        <button type="button" onClick={() => onSearch(value)}>
+          Search
+        </button>
+      </div>
       <div>
         {cameras
-          .filter((val) => {
-            return val.model.toLowerCase().includes(searchTerm.toLowerCase());
-          })
-          .map((val, index) => (
-            <>
-              {searchTerm && (
-                <button
-                  type="button"
-                  key={val.id}
-                  value={val.id}
-                  onClick={(e) => handleDisplay(e, val)}
-                >
-                  {val.model}
-                </button>
-              )}
-              {display && (
-                <div key={val}>
-                  <h2>{val.brand}</h2>
-                  <p>Modèle : {val.model}</p>
-                  <p>{val.sensor}</p>
-                  {val.sensor === "" || val.sensor === null ? (
-                    <p />
-                  ) : (
-                    <p>Capteur : {val.sensor}</p>
-                  )}
-                  <p>Type de capteur : {val.type}</p>
-                  <p>Largeur du capteur : {val.lengthmm}</p>
-                  <p>Hauteur du capteur : {val.heightmm}</p>
-                  <p>Largeur en pixel du capteur : {val.lengthpix}</p>
-                  <p>Hauteur en pixel du capteur : {val.heightpix}</p>
-                  <p>Photosites : {val.photosites}</p>
-                  <p>Megapixels : {val.megapixels}</p>
-                  {val.cadence === 0 || val.cadence === null ? (
-                    <p />
-                  ) : (
-                    <p>Cadence : {val.cadence} images par seconde</p>
-                  )}
-                  {val.dynamic === 0 || val.dynamic === null ? (
-                    <p />
-                  ) : (
-                    <p>Dynamique : {val.dynamic}</p>
-                  )}
-                  {val.bits === 0 || val.bits === null ? (
-                    <p />
-                  ) : (
-                    <p>Bits : {val.bits}</p>
-                  )}
-                  {val.readNoise === 0 || val.readNoise === null ? (
-                    <p />
-                  ) : (
-                    <p>Bruit de lecture : {val.readNoise}</p>
-                  )}
-                  {val.darkCurrent === 0 || val.darkCurrent === null ? (
-                    <p />
-                  ) : (
-                    <p>Courant d'obscurité : {val.darkCurrent}</p>
-                  )}
-                  {val.readTime === 0 || val.readTime === null ? (
-                    <p />
-                  ) : (
-                    <p>Temps de lecture du capteur : {val.readTime}</p>
-                  )}
-                  {val.capacity === 0 || val.capacity === null ? (
-                    <p />
-                  ) : (
-                    <p>Capacité des pixels : {val.capacity}</p>
-                  )}
-                  {val.cooler === "" || val.cooler === null ? (
-                    <p />
-                  ) : (
-                    <p>Refroidissement : -{val.cooler}°C</p>
-                  )}
-                </div>
-              )}
-            </>
-          ))}
-      </div>
+          .filter((item) => {
+            const searchTerm = value.toLowerCase();
+            const model = item.model.toLowerCase();
 
-      {/* {cameras.map((data) => (
-        <ul key={data.id}>
-          <li>
-            <h2>{data.brand}</h2>
-            <p>Modèle : {data.model}</p>
-            {data.sensor === "" || data.sensor === null ? (
-              <p />
-            ) : (
-              <p>Capteur : {data.sensor}</p>
-            )}
-            <p>Type de capteur : {data.type}</p>
-            <p>Largeur du capteur : {data.lengthmm}</p>
-            <p>Hauteur du capteur : {data.heightmm}</p>
-            <p>Largeur en pixel du capteur : {data.lengthpix}</p>
-            <p>Hauteur en pixel du capteur : {data.heightpix}</p>
-            <p>Photosites :  {data.photosites}</p>
-            <p>Megapixels : {data.megapixels}</p>
-            {data.cadence === 0 || data.cadence === null ? (
-              <p />
-            ) : (
-              <p>Cadence : {data.cadence} images par seconde</p>
-            )}
-            {data.dynamic === 0 || data.dynamic === null ? (
-              <p />
-            ) : (
-              <p>Dynamique : {data.dynamic}</p>
-            )}
-            {data.bits === 0 || data.bits === null ? (
-              <p />
-            ) : (
-              <p>Bits : {data.bits}</p>
-            )}
-            {data.readNoise === 0 || data.readNoise === null ? (
-              <p />
-            ) : (
-              <p>Bruit de lecture : {data.readNoise}</p>
-            )}
-            {data.darkCurrent === 0 || data.darkCurrent === null ? (
-              <p />
-            ) : (
-              <p>Courant d'obscurité : {data.darkCurrent}</p>
-            )}
-            {data.readTime === 0 || data.readTime === null ? (
-              <p />
-            ) : (
-              <p>Temps de lecture du capteur : {data.readTime}</p>
-            )}
-            {data.capacity === 0 || data.capacity === null ? (
-              <p />
-            ) : (
-              <p>Capacité des pixels : {data.capacity}</p>
-            )}
-            {data.cooler === "" || data.cooler === null ? (
-              <p />
-            ) : (
-              <p>Refroidissement : -{data.cooler}°C</p>
-            )}
-            <button
-              type="button"
-              onClick={() => navigate(`/administration/edit-event/${data.id}`)}
-            >
-              Modifier la camera
-            </button>
-            <button type="submit" onClick={() => deleteCamera(data.id)}>
-              Supprimer la camera {data.modele}
-            </button>
-          </li>
-        </ul>
-      ))} */}
+            return (
+              searchTerm && model.startsWith(searchTerm) && model !== searchTerm
+            );
+          })
+          .slice(0, 10)
+          .map((item) => (
+            // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions
+            <div key={item.id} onClick={() => onSearch(item.model)}>
+              {item.model}
+            </div>
+          ))}
+        <div>{write}</div>
+      </div>
     </div>
   );
 }
